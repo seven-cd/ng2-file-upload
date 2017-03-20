f06cf9e5d1bbe6ab88f8b96301a1c95107f32e43
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2017-03-20 13:45:34 +0100

    Merge dev

--- components/file-upload/file-details.class.js
+++ components/file-upload/file-details.class.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var FileDetails = (function () {
     function FileDetails(file) {
         this.file = file;
--- components/file-upload/file-like-object.class.js
+++ components/file-upload/file-like-object.class.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function isElement(node) {
     return !!(node && (node.nodeName || node.prop && node.attr && node.find));
 }
