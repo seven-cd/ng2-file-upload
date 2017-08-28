166e0dfe0cb08b52de9871bd91a2f9a9e58ef1df
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2017-08-28 12:30:14 +0200

    Dunno

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export {FileSelectDirective} from './src/file-select.directive';
 export {FileDropDirective} from './src/file-drop.directive';
 export {ImagePreviewDirective} from './src/image-preview.directive';
 export {FileUploader} from './src/file-uploader.class';
+export {FileUploadModule} from './file-upload.module';
